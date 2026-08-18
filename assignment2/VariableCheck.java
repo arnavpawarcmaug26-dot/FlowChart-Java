837d119a80d54683d32b46a150ff920524de2524
arnavpawarcmaug26-dot <arnav.pawar.cmaug26@gmail.com>
2026-08-18 17:29:11 +0530

    VariableCheck

---
+++ assignment2/VariableCheck.java
@@ -0,0 +1,21 @@
+public class VariableCheck {
+    static byte a;
+    static short b;
+    static int c;
+    static long d;
+    static float e;
+    static double f;
+    static char g;
+    static boolean h;
+
+    public static void main(String[] args) {
+        System.out.println("byte value: " + a);
+        System.out.println("short value: " + b);
+        System.out.println("int value: " + c);
+        System.out.println("long value: " + d);
+        System.out.println("float value: " + e);
+        System.out.println("double value: " + f);
+        System.out.println("char value: [" + g + "]");
+        System.out.println("boolean value: " + h);
+    }
+}
